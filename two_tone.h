597f9cd98adbb1d4e73f831349237585c6ae9c06
bluetailtech <bluetailtech@gmail.com>
2022-12-22 18:14:55 -0800

    update

--- two_tone.h
+++ two_tone.h
@@ -1,18 +1,11 @@
 #include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
 typedef struct {
   uint8_t tone_idx;
   float tone_freq_hz;
 } two_tone_table;
 
-int two_tone_get_idx( int zero_cross );
 
-
-#ifdef __cplusplus
-}
-#endif
+int two_tone_get_idx(float measured_freq_hz);
+void process_tone_pair_qcii( int TONE_A, int TONE_B );
+float two_tone_get_freq(int idx);
